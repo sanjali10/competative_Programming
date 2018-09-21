eda72b1013fcf9db2ba938d2110cbbaa2e29b49a
Anjali Sharma <sanjali@ssdiblr.sharp.co.in>
2018-09-21 10:08:56 +0530

    Prime series upto n userdefined number

---
+++ primeSeries.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main
+{
+	    public static void main(String[] args)
+	    {
+	        int flag=0,check,j,i;
+	        int[] temps = new int[100];
+	        System.out.println("enter the number");
+         	Scanner input = new Scanner(System.in);
+            int number = input.nextInt();
+			System.out.println("The Prime numbers are:");
+	     	for(i=2;i<=number;i++)
+	     	{
+	     	    flag=0;
+		       for(j=2;j<i;j++)
+		      {
+		          if(i%j==0)
+		         {
+		            flag=1;
+		            break;
+		         }
+		      }
+		      if(flag==0)
+	     	{
+	     	    System.out.println(i);
+	     	}
+		      
+	     	}
+	     	
+		
+	}
+}
